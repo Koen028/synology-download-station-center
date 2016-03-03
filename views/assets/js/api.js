33a7b9aea0c999ac9536eec7552cb085e54ff827
Koen Hendriks <koen.hendriks@amicoservices.nl>
2016-03-03 16:20:52 +0100

    Added Task API's Resume, Pause and Delete

--- views/assets/js/api.js
+++ views/assets/js/api.js
@@ -7,25 +7,55 @@ const baseUrl = auth.baseurl;
 var sid = null;
 
 module.exports = {
-  authAPI: function(callback){
+  authAPI: function(callback) {
     $.get(baseUrl + "auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=" + username + "&passwd=" + password + "&session=DownloadStation&format=sid", function(res) {
       var json = JSON.parse(res);
       sid = json.data.sid;
-      if(typeof callback === "function"){
+      if (typeof callback === "function") {
         callback();
       }
     });
   },
-  getSID: function(){
+  getSID: function() {
     return sid
   },
-  getList: function(callback){
-    var data = null;
-    $.get(baseUrl + "DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=list&_sid=" + sid, function(res){
-      data = res;
-      if(typeof callback === "function"){
-        callback(data);
-      }
-    });
+  tasks: {
+    getList: function(callback) {
+      var data = null;
+      $.get(baseUrl + "DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=list&additional=transfer&_sid=" + sid, function(res) {
+        data = res;
+        if (typeof callback === "function") {
+          callback(data);
+        }
+      });
+    },
+    resume: function(id, callback){
+      var data = null;
+      $.get(baseUrl + "DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=resume&id=" + id +"&_sid=" + sid, function(res){
+        data = res;
+        if(typeof callback === "function"){
+          callback(data);
+        }
+      });
+    },
+    pause: function(id, callback){
+      var data = null;
+      $.get(baseUrl + "DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=pause&id=" + id +"&_sid=" + sid, function(res){
+        data = res;
+        if(typeof callback === "function"){
+          callback(data);
+        }
+      });
+    },
+    delete: function(id, force_complete, callback){
+      force_complete = force_complete || false;
+      var data = null;
+      $.get(baseUrl + "DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=delete&id=" + id +"&force_complete=" + force_complete + "&_sid=" + sid, function(res){
+        data = res;
+        if(typeof callback === "function"){
+          callback(data);
+        }
+      });
+    }
   }
 }
