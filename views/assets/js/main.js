383ce598078c1bce44304864f807ccbf72cef3b0
Koen Hendriks <koen.hendriks@amicoservices.nl>
2016-03-03 13:35:03 +0100

    Changed Init functions

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -4,18 +4,29 @@ const api = require("./api.js");
 
 $(function(){
   api.authAPI(function(){
-    api.getList(function(data){
-      var tasks = JSON.parse(data).data.tasks;
-      $("#menu .count").html(tasks.length);
-      $.each(tasks, function(index, value){
-        var template = $("#item-template").html();
-        Mustache.parse(template);
-        var rendered = Mustache.render(template, tasks[index]);
-        $("#content").append(rendered);
-      });
-    });
+    init();
   });
+
   $(".icon-settings").click(function(){
     // TODO: Navigate to Settings page
   });
 });
+
+function init(){
+  api.getList(function(data){
+    updateTasksList(data);
+  });
+}
+
+function updateTasksList(data){
+  var tasks = JSON.parse(data).data.tasks;
+  $("#menu .count").html(tasks.length);
+  // empty current list
+  $("#content").empty();
+  $.each(tasks, function(index, value){
+    var template = $("#item-template").html();
+    Mustache.parse(template);
+    var rendered = Mustache.render(template, tasks[index]);
+    $("#content").append(rendered);
+  });
+}
